c8ac8050a70ec833b519fb3a39533c7471fc0c8f
nclmr <s1083490@studenti.univpm.it>
2022-02-20 17:12:14 +0100

    Chatbot con sport e calcio.

--- actions/actions.py
+++ actions/actions.py
@@ -56,34 +56,6 @@ class ActionUtterProva(Action):
     
         dispatcher.utter_message(text=output)
         return []
-
-class ActionUtterChoice(Action):
-    def name(self) -> Text:
-        return "action_utter_choice"
-        
-    def run(self, dispatcher: CollectingDispatcher,tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        
-        specific_choice = str(tracker.get_slot('scelta'))
-        print(specific_choice)
-        if name_global == 'film' or name_global == 'movie' :
-            if specific_choice == "particular film" or specific_choice == "particular movie":
-                dispatcher.utter_message(response="utter_choice", name=specific_choice)
-            elif specific_choice == "top trends" or specific_choice == "top charts":
-                specific_choice = specific_choice + 'film'
-                dispatcher.utter_message(response="utter_choice", name=specific_choice)
-            else:
-                dispatcher.utter_message(text="Sorry, I don't understand film")
-        elif name_global== 'tv series':
-            if specific_choice == "particular tv series":
-                dispatcher.utter_message(response="utter_choice", name=specific_choice)
-            elif specific_choice == "top trends" or specific_choice == "top charts":
-                specific_choice = specific_choice + 'tv series'
-                dispatcher.utter_message(response="utter_choice", name=specific_choice)
-            else:
-                dispatcher.utter_message(text="Sorry, I don't understand tv series")
-        else:
-            dispatcher.utter_message(text="Errorrrrr")
-        return []
     
 class ActionParticular(Action):
     def name(self) -> Text:
@@ -94,9 +66,41 @@ class ActionParticular(Action):
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         link = []
         title = []
-        particular = str(tracker.get_slot('film'))
+        particular = str(tracker.get_slot('squadra'))
         output="you chose {}".format(particular)
         url = "https://www.sportmediaset.mediaset.it/calcio/{}/".format(particular.lower())
+        try:
+            re = requests.get(url)
+            soup = BeautifulSoup(re.content, 'html.parser')
+        
+            paging = soup.find_all("h2", {'class':"article-heading"})
+            for a in soup.find_all("a", {'data-urltype':"alltitle", 'href' : True}):
+                link.append(a['href'])
+        
+            for tag in paging:
+                title.append(tag.get_text().strip())
+            link = link[1:11]
+            title = title[1:11]
+            for i in range(0,10):
+                output1 = "News: {}, Link: {}".format(title[i], link[i])
+                dispatcher.utter_message(text=output1)
+        except:
+            output = "Write team name correctly"
+            dispatcher.utter_message(text=output)
+        return []
+
+class ActionParticularTopNewsFootball(Action):
+    def name(self) -> Text:
+        return "action_particular_football_topnews"
+        
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        link = []
+        title = []
+        particular = str(tracker.get_slot('squadra'))
+        output="you chose {}".format(particular)
+        url = "https://www.sportmediaset.mediaset.it/calcio/".format(particular.lower())
         re = requests.get(url)
         soup = BeautifulSoup(re.content, 'html.parser')
         paging = soup.find_all("h2", {'class':"article-heading"})
@@ -114,30 +118,34 @@ class ActionParticular(Action):
 
 class ActionParticularTopNews(Action):
     def name(self) -> Text:
-        return "action_particular_football_topnews"
+        return "action_particular_topnews"
         
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         link = []
         title = []
-        particular = str(tracker.get_slot('film'))
-        output="you chose {}".format(particular)
-        url = "https://www.sportmediaset.mediaset.it/calcio/".format(particular.lower())
+        particular = str(tracker.get_slot('choice'))
+        url = "https://www.sportmediaset.mediaset.it/{}/".format(particular.lower())
         re = requests.get(url)
-        soup = BeautifulSoup(re.content, 'html.parser')
-        paging = soup.find_all("h2", {'class':"article-heading"})
-        for a in soup.find_all("a", {'data-urltype':"alltitle", 'href' : True}):
-            link.append(a['href'])
+        try:
+            soup = BeautifulSoup(re.content, 'html.parser')
+            paging = soup.find_all("h2", {'class':"article-heading"})
+            for a in soup.find_all("a", {'data-urltype':"alltitle", 'href' : True}):
+                link.append(a['href'])
         
-        for tag in paging:
-            title.append(tag.get_text().strip())
-        link = link[1:11]
-        title = title[1:11]
-        for i in range(0,10):
-            output1 = "News: {}, Link: {}".format(title[i], link[i])
-            dispatcher.utter_message(text=output1)
+            for tag in paging:
+                title.append(tag.get_text().strip())
+            link = link[1:11]
+            title = title[1:11]
+            for i in range(0,10):
+                output1 = "News: {}, Link: {}".format(title[i], link[i])
+                dispatcher.utter_message(text=output1)
+        except:
+            output = "Write sport name correctly"
+            dispatcher.utter_message(text=output)
         return []
+
 
 
 
